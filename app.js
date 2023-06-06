334e1b12a1d5a563f7fbc2068434bb8e3a296dad
TarıkSogukpinar <tariksogukpinar@outlook.com>
2023-06-06 05:51:41 +0300

    docker files created

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 //npm packages
-import express from 'express';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 
 
 //Custom Modules,Packages,Configs,Etc
+import connectionDatabase from "./helpers/connectionDatabase/connectionDatabase.js";
+import corsOption from "./helpers/cors/corsOption.js";
 
 const envFile =
   process.env.NODE_ENV === "production"
@@ -19,13 +23,22 @@ dotenv.config({
 
 const app = express();
 
-
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(compression());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(cors(corsOption));
 
+
+
+app.get("/", (req, res) => {
+    res.send("Health Check");
+});
+
+await connectionDatabase();
 
 export const PORT = process.env.PORT || 5000;
 export default app;
