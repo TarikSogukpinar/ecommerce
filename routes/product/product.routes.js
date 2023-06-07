e8f1553c2549cbd2e16747aab8e20ebd77a5b570
TarıkSogukpinar <tariksogukpinar@outlook.com>
2023-06-07 13:30:24 +0300

    updateProductController created

--- routes/product/product.routes.js
+++ routes/product/product.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { verifyToken } from "../../middleware/verifyTokens/verifyTokens.js";
 import { verifyRoles } from "../../middleware/verifyRoles/verifyRoles.js";
 import createProductController from "../../controllers/product/createProductController.js";
-
+import updateProductController from "../../controllers/product/updateProductController.js";
 const router = Router();
 
 router.post(
@@ -10,6 +10,13 @@ router.post(
   verifyToken,
   verifyRoles("seller"),
   createProductController.createProduct
+);
+
+router.put(
+  "/updateProduct/:id",
+  verifyToken,
+  verifyRoles("seller"),
+  updateProductController.updateProduct
 );
 
 export default router;
